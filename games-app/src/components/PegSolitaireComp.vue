c61cbaf7036ae9b26f78effc044b0768a9a566a0
nickforster <nick_forster@icloud.com>
2022-12-23 13:31:03 +0100

    pegSolitaire game setup

---
+++ games-app/src/components/PegSolitaireComp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="game">
+    <div v-for="(field, index) in fields" :key="index">
+      <div class="field">{{ field }}</div>
+    </div>
+  </div>
+  <button id="back-button"><a href="#">back</a></button>
+  <button id="reset-button" @click="resetGame">reset</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      fields: []
+    }
+  },
+  methods: {
+    resetGame() {
+
+    }
+  }
+}
+</script>
+<style>
+
+</style>
