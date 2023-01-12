b93e0084199b040213518443373b5edfe880bd55
nickforster <nick_forster@icloud.com>
2023-01-12 10:58:44 +0100

    PacmanComp setup

---
+++ games-app/src/components/PacmanComp.vue
@@ -0,0 +1,71 @@
+<template>
+  <canvas id="game" width="450" height="450"></canvas>
+  <button id="back-button"><a href="#">back</a></button>
+  <button id="reset-button" @click="resetGame">reset</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      context: "",
+    }
+  },
+  methods: {
+    resetGame() {
+      this.context = document.getElementById("game").getContext("2d")
+    }
+  },
+  mounted() {
+    this.resetGame()
+  }
+}
+</script>
+<style scoped>
+#game {
+  border: 1px solid #1b0d31;
+}
+
+#back-button, #reset-button {
+  background-color: #090a1a;
+  color: #492386;
+  margin: 0;
+  text-transform: uppercase;
+  user-select: none;
+  position: absolute;
+  width: fit-content;
+  height: fit-content;
+  border: 1px solid #492386;
+  border-radius: 6px;
+  padding: .4rem;
+  font-size: 1rem;
+  top: 1rem;
+  left: 1rem;
+}
+
+a {
+  color: #492386;
+  background: none;
+  text-decoration: none;
+}
+
+a:hover, a:active, a:visited {
+  color: #492386;
+}
+
+#reset-button {
+  bottom: 5%;
+  left: 50%;
+  transform: translateX(-50%);
+  top: auto;
+}
+
+@media (max-height: 450px) {
+  #reset-button {
+    bottom: 0;
+  }
+}
+
+button:hover {
+  background-color: #2c1550 !important;
+}
+</style>
